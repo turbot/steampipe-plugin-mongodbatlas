363649478018b49ee790757c77638dbcd2136d4f
Binaek Sarkar <binaek@turbot.com>
2022-05-11 22:02:48 +0530

    project table

--- mongodbatlas/table_mongodbatlas_project.go
+++ mongodbatlas/table_mongodbatlas_project.go
@@ -6,7 +6,6 @@ import (
 	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
 	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
-	"go.mongodb.org/atlas/mongodbatlas"
 )
 
 func tableAtlasProject(_ context.Context) *plugin.Table {
@@ -15,16 +14,6 @@ func tableAtlasProject(_ context.Context) *plugin.Table {
 		Description: "",
 		List: &plugin.ListConfig{
 			Hydrate: listAtlasProjects,
-			KeyColumns: []*plugin.KeyColumn{
-				{
-					Name:    "org_id",
-					Require: plugin.Optional,
-				},
-			},
-		},
-		Get: &plugin.GetConfig{
-			Hydrate:    getAtlasProject,
-			KeyColumns: plugin.SingleColumn("id"),
 		},
 		Columns: []*plugin.Column{
 			{
@@ -64,97 +53,20 @@ func tableAtlasProject(_ context.Context) *plugin.Table {
 }
 
 func listAtlasProjects(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	// Create client
 	client, err := getMongodbAtlasClient(ctx, d)
 	if err != nil {
 		plugin.Logger(ctx).Error("mongodbatlas_project.listAtlasProjects", "connection_error", err)
 		return nil, err
 	}
-	// Retrieve the list of incidents
-	itemsPerPage := int64(100)
-	// Reduce the basic request limit down if the user has only requested a small number of rows
-	if d.QueryContext.Limit != nil && *d.QueryContext.Limit < itemsPerPage {
-		itemsPerPage = *d.QueryContext.Limit
-	}
-
-	pageNumber := 1
-	orgId := ""
-
-	// Additional Filters
-	if d.KeyColumnQuals["org_id"] != nil {
-		orgId = d.KeyColumnQuals["org_id"].GetStringValue()
-	}
-
-	for {
-		projects, _, err := fetchProjectsPage(ctx, client, pageNumber, itemsPerPage, orgId)
-
-		if err != nil {
-			plugin.Logger(ctx).Error("mongodbatlas_project.listAtlasProjects", "query_error", err)
-			return nil, err
-		}
-
-		for _, project := range projects.Results {
-			d.StreamListItem(ctx, project)
-
-			// Context can be cancelled due to manual cancellation or the limit has been hit
-			if d.QueryStatus.RowsRemaining(ctx) == 0 {
-				return nil, nil
-			}
-		}
-
-		// find the next page
-		hasNextPage := false
-		for _, link := range projects.Links {
-			if link.Rel == "next" {
-				hasNextPage = true
-				break
-			}
-		}
-
-		if hasNextPage {
-			pageNumber++
-			continue
-		}
-
-		break
-	}
-
-	return nil, nil
-}
-
-func fetchProjectsPage(ctx context.Context, client *mongodbatlas.Client, pageNumber int, itemsPerPage int64, orgId string) (*mongodbatlas.Projects, *mongodbatlas.Response, error) {
-	if len(orgId) != 0 {
-		plugin.Logger(ctx).Trace("mongodbatlas_project.listAtlasProjects", "all_projects")
-		return client.Organizations.Projects(ctx, orgId, &mongodbatlas.ListOptions{
-			PageNum:      pageNumber,
-			ItemsPerPage: int(itemsPerPage),
-		})
-	}
-
-	plugin.Logger(ctx).Trace("mongodbatlas_project.listAtlasProjects", "org_projects", orgId)
-	return client.Projects.GetAllProjects(ctx, &mongodbatlas.ListOptions{
-		PageNum:      pageNumber,
-		ItemsPerPage: int(itemsPerPage),
-	})
-}
-
-func getAtlasProject(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	client, err := getMongodbAtlasClient(ctx, d)
-	if err != nil {
-		plugin.Logger(ctx).Error("mongodbatlas_project.listAtlasProjects", "connection_error", err)
-		return nil, err
-	}
-	id := d.KeyColumnQuals["id"].GetStringValue()
-
-	// No inputs
-	if len(id) == 0 {
-		return nil, nil
-	}
+	config := GetConfig(d.Connection)
+	id := *config.ProjectId
 
 	project, _, err := client.Projects.GetOneProject(ctx, id)
 	if err != nil {
 		return nil, err
 	}
 
-	return project, nil
+	d.StreamListItem(ctx, project)
+
+	return nil, nil
 }
