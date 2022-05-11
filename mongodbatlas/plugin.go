55fd8db5b43bd574855acca120e7f056fc94dc8c
Binaek Sarkar <binaek@turbot.com>
2022-05-11 22:03:31 +0530

    hooking up tables

--- mongodbatlas/plugin.go
+++ mongodbatlas/plugin.go
@@ -20,7 +20,12 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			Schema:      ConfigSchema,
 		},
 		TableMap: map[string]*plugin.Table{
-			"mongodbatlas_project": tableAtlasProject(ctx),
+			"mongodbatlas_project":                tableAtlasProject(ctx),
+			"mongodbatlas_org":                    tableAtlasOrg(ctx),
+			"mongodbatlas_cluster":                tableAtlasCluster(ctx),
+			"mongodbatlas_advanced_cluster":       tableAtlasAdvancedCluster(ctx),
+			"mongodbatlas_serverless_instance":    tableAtlasServerlessInstance(ctx),
+			"mongodbatlas_project_ip_access_list": tableAtlasProjectIpAccessList(ctx),
 		},
 	}
 
