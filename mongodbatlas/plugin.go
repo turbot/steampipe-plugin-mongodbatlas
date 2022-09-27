baf4c96193e92a9089b5e0733c2df18efafa8641
Ved misra <47312748+misraved@users.noreply.github.com>
2022-09-27 12:32:27 +0530

    Recompile plugin with steampipe-plugin-sdk v4.1.7 and Go version 1.19 (#5)

--- mongodbatlas/plugin.go
+++ mongodbatlas/plugin.go
@@ -3,8 +3,8 @@ package mongodbatlas
 import (
 	"context"
 
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin/transform"
 	"go.mongodb.org/atlas/mongodbatlas"
 )
 
